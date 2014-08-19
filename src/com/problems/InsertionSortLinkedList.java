be64b5cb325abf00f65edfe7655079beeb1faaa5
houzhaowei <ninja.hzw@gmail.com>
2014-08-19 09:26:38 -0400

    add code

---
+++ src/com/problems/InsertionSortLinkedList.java
@@ -0,0 +1,90 @@
+package com.problems;
+
+import java.util.LinkedList;
+
+/**
+ * !! Important
+ * Not good at LinkedList, I will review this class frequently.
+ *
+ * Created by houzhaowei on 8/16/14.
+ */
+public class InsertionSortLinkedList {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+//    public ListNode insertionSortList(ListNode head) {
+//
+//        int sortedCount = 0;
+//
+//        // one element list
+//        if (head.next == null){
+//            return head;
+//        }
+//        ListNode current = head.next;
+//        ListNode traverser = head;
+//        ListNode pre = new ListNode(0);
+//        pre.next = head;
+//        while (current != null){
+//            int index = 0;
+//            while (traverser != null){
+//                if (current.val < traverser.val && index <= sortedCount){
+//                    pre.next = current;
+//                    current.next = traverser;
+//                }
+//                sortedCount ++;
+//            }
+//        }
+//
+//    }
+
+
+    /**
+     * Other people's solution, which seems simpler.
+     * @param head
+     * @return
+     */
+    public ListNode insertionSortList(ListNode head) {
+        // this helper always pointed to the first element.
+        ListNode helper=new ListNode(0);
+        ListNode pre=helper;
+        ListNode current=head;
+        while(current!=null) {
+            pre=helper;
+            while(pre.next!=null&&pre.next.val<current.val) {
+                pre=pre.next;
+            }
+
+            /**
+             * !!
+             * the following code segment shows how to insert nodes in linked list!
+             */
+            ListNode next=current.next;
+            current.next=pre.next;
+            pre.next=current;
+            current=next;
+        }
+        return helper.next;
+    }
+
+    public static void main(String[] args){
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(3);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        ListNode node = new InsertionSortLinkedList().insertionSortList(node1);
+
+
+    }
+}
---
+++ src/com/problems/MaxPointsOnALine.java
@@ -0,0 +1,74 @@
+package com.problems;
+
+import java.util.Random;
+
+/**
+ * Created by houzhaowei on 8/15/14.
+ *
+ * Hint:
+ * Calculate the slope of any two points, and group the same slopes.
+ * count the maximum same slopes.
+ *
+ *
+ */
+public class MaxPointsOnALine {
+
+
+    private static class Point {
+         int x;
+         int y;
+         Point() { x = 0; y = 0; }
+         Point(int a, int b) { x = a; y = b; }
+    }
+
+    private class Pair{
+
+    }
+
+    public int maxPoints(Point[] points) {
+
+        long[] pairs = new long[points.length * points.length];
+        int maxCount = 0;
+
+        for (Point outterPoint : points){
+            for (Point innerPoint: points){
+                if (outterPoint != innerPoint){
+                    int distanceX = innerPoint.x - outterPoint.x;
+                    int distanceY = innerPoint.y - outterPoint.y;
+
+                    /**
+                     *  in case that two points are the same, then do not pair them
+                     *  instead, we need to add one to
+                     */
+                    if (distanceX == 0 && distanceY == 0){
+                        maxCount ++;
+                    }
+
+                    double gradient;
+                    if (distanceX == 0) {
+                        gradient = Double.POSITIVE_INFINITY;
+                    }
+                    if (distanceY == 0) {
+                        gradient = 0;
+                    }
+                    gradient = (innerPoint.x - outterPoint.x)/(innerPoint.y - outterPoint.y);
+//                    pairs[index] = gradient;
+//                    index ++;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        Point[] points = new Point[20];
+        Random r = new Random();
+        for (int i = 0 ; i < 20; i ++){
+            points[i] = new Point(r.nextInt(10),r.nextInt(10));
+        }
+
+        new MaxPointsOnALine().maxPoints(points);
+    }
+
+}
